51a88e82ff10e5a818936af74406b4518b7f1f9c
Szuszkiewicz <yck920601@gmail.com>
2026-05-02 17:16:45 +0800

    增加文章和删除文章

--- dtc-comment-main/src/main/java/com/Peter/CommentApplication.java
+++ dtc-comment-main/src/main/java/com/Peter/CommentApplication.java
@@ -1,12 +1,14 @@
 package com.Peter;
 
+import com.Peter.Param.BaseResult;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.Peter")
 public class CommentApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(CommentApplication.class, args);
+    public static void main(){
+
     }
-}
+
+    }
