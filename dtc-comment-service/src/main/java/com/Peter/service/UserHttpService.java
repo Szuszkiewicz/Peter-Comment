a1066d6d368be6d99f91392ee03b8877109bfbc1
Szuszkiewicz <yck920601@gmail.com>
2026-05-21 23:14:27 +0800

    2026年5月21日，文章点赞

---
+++ dtc-comment-service/src/main/java/com/Peter/service/UserHttpService.java
@@ -0,0 +1,24 @@
+package com.Peter.service;
+
+import com.Peter.Param.BaseResult;
+import com.Peter.api.UserFeignService;
+import com.Peter.dto.UserInfoDto;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserHttpService {
+    @Resource
+    private UserFeignService userFeignService;;
+
+    public UserInfoDto getUserInfo(Long id){
+        if(id == null){
+            return null;
+        }
+        BaseResult<UserInfoDto> userInfoDtoBaseResult = userFeignService.queryUserInfo(String.valueOf(id));
+        if(!userInfoDtoBaseResult.getSuccess()){
+            return null;
+        }
+        return userInfoDtoBaseResult.getData();
+    }
+}
